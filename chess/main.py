f25b20eb089bc08792824386508c7700d7c70f65
TravisRobson <travisjrobson@gmail.com>
2022-07-31 16:51:28 -0700

    pre-commit

--- chess/main.py
+++ chess/main.py
@@ -12,8 +12,17 @@ from opponent import opponent_choose_move
 
 
 def ask_user_for_move(valid_moves):
-    move_string_invalidated = input("What move do you want to play?")
+    move_string_invalidated = input("What move do you want to play? ")
     return move_string_invalidated
+
+
+def switch_turns(whose_turn):
+    if whose_turn == Player.WHITE:
+        print("Black's turn.")
+        return Player.BLACK
+    else:
+        print("White's turn.")
+        return Player.WHITE
 
 
 def main():
@@ -22,23 +31,23 @@ def main():
     print("White's turn.")
 
     whose_turn = Player.WHITE
+    infinite_loop_counter = 0
+    # One move per loop
     while not is_game_over(board, whose_turn):
-
         valid_moves = find_valid_moves(board, whose_turn)
         if whose_turn == Player.WHITE:
             move = ask_user_for_move(valid_moves)
         else:
-            move = opponent_choose_move(valid_moves)
+            move = opponent_choose_move(board, valid_moves)
 
         board = update_board(board, move)
         display_board(board)
 
-        if whose_turn == Player.WHITE:
-            whose_turn = Player.BLACK
-            print("Black's turn.")
-        else:
-            whose_turn = Player.WHITE
-            print("White's turn.")
+        whose_turn = switch_turns(whose_turn)
+
+        infinite_loop_counter += 1
+        if infinite_loop_counter > 500:
+            raise RuntimeError("Infinite loop detected.")
 
 
 if __name__ == "__main__":
--- chess/moves.py
+++ chess/moves.py
@@ -1,13 +1,84 @@
+from chesstypes import Piece, Player
+
+
 def is_game_over(board, whose_turn):
     # TODO implement
-    return True
-
-
-def find_valid_moves(board, whose_turn):
-    # TODO implement
-    return []
+    return False
 
 
 def update_board(board, move):
     # TODO implement
     return board
+
+
+def __find_valid_king_moves(postion, board):
+    # TODO implement
+    moves = []
+
+    return moves
+
+
+def __find_valid_queen_moves(postion, board):
+    # TODO implement
+    moves = []
+
+    return moves
+
+
+def __find_valid_rook_moves(postion, board):
+    # TODO implement
+    moves = []
+
+    return moves
+
+
+def __find_valid_bishop_moves(postion, board):
+    # TODO implement
+    moves = []
+
+    return moves
+
+
+def __find_valid_knight_moves(postion, board):
+    # TODO implement
+    moves = []
+
+    return moves
+
+
+def __find_valid_pawn_moves(postion, board):
+    # TODO implement
+    moves = []
+
+    return moves
+
+
+def find_valid_moves(board, whose_turn):
+    if whose_turn == Player.WHITE:
+        pieces = board.white_pieces
+        positions = board.white_positions
+    else:
+        pieces = board.black_pieces
+        positions = board.black_positions
+
+    moves = []
+    for piece, position in zip(pieces, positions):
+        if piece == Piece.KING:
+            moves.append(__find_valid_king_moves(position, board))
+        elif piece == Piece.QUEEN:
+            moves.append(__find_valid_queen_moves(position, board))
+        elif piece == Piece.ROOK:
+            moves.append(__find_valid_rook_moves(position, board))
+        elif piece == Piece.BISHOP:
+            moves.append(__find_valid_bishop_moves(position, board))
+        elif piece == Piece.KNIGHT:
+            moves.append(__find_valid_knight_moves(position, board))
+        elif piece == Piece.PAWN:
+            moves.append(__find_valid_pawn_moves(position, board))
+        else:
+            raise TypeError(f"Piece has invalid type {piece}")
+
+    # TODO Remove any moves that puts current player's king in check
+    # TODO Are there any stalemate considerations?
+
+    return moves
